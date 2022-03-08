08e8b278bec941b9301f72e15c67b2750a58c54d
Alberto <albetoiaa@gmail.com>
2022-03-08 15:44:52 +0100

    First commit

---
+++ hardhat.config.js
@@ -0,0 +1,14 @@
+require("@nomiclabs/hardhat-waffle");
+require("dotenv").config();
+
+module.exports = {
+  solidity: "0.8.4",
+  networks: {
+    rinkeby: {
+      url: process.env.STAGING_ALCHEMY_KEY,
+      accounts: [process.env.PRIVATE_KEY],
+    },
+  },
+};
+
+//My contract: 0x289A617a37359dF632B36f3cd967125eAe85AA34
